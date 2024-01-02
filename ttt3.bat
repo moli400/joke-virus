18e910aecc2514c85e9568ed161726c6ba806e56
moli400 <152659961+moli400@users.noreply.github.com>
2024-01-02 11:57:13 -0300

    Create ttt3.bat

---
+++ ttt3.bat
@@ -0,0 +1,35 @@
+
+@echo off
+title arquivos em analize . . .
+color 4
+
+echo ................................................................................
+echo  24/03/2023           TK KILER ATIVADO 
+echo ................................................................................
+echo          .AMMMMMMMMMMA.          
+echo       .AV. :::.:.:.::MA.        
+echo      A' :.. : .:`A       
+echo     A'.. . `A.     
+echo    A' :. ::::::::: : :`A    
+echo    M. :::.:.:.::: . .M    
+echo    MILÍMETROS    
+echo    V : :.::.:........:.: :V    
+echo   AA: ..:...:...:. AA   
+echo  .V MA:.....:M.::.::. .:AM.M   
+echo A' .VMMMMMMMM:.:AMMMMMMMV: A  
+echo:M . .`VMMMMMMV.:A `VMMMMV .:M:
+echo V.:. ..`VMMMV.:AM..`VMV' .:V  
+echo  V. .:. .....:AMMA. . .:. .V   
+echo   VMM...: ...:.MMMM.: .: MMV    
+echo       `MV: . ..M.:M..:::M'      
+echo         `M::. .:.... .::M       
+echo          M:. :. .... ..M       
+echo           VK V: M:. M.:M.V       
+echo          `V.:M.. M.:MV'
+echo ................................................................................
+echo 
+echo ................................................................................
+tree
+
+pause
+
